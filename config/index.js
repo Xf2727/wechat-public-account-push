684310ad812347cee1a476d4a3d0bbf6a483ed9e
Xf2727 <114224601+Xf2727@users.noreply.github.com>
2022-09-23 18:15:07 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "o6z1n6_-At923TdFLoZloOfj5krM",
+      id: "o6z1n6xnH9UYBUVegF3H4MtMUNpE",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "OKtxBEzByjA8a0P9-dzPieTtenSo3jP6PMUG_2lstvQ",
       // 所在省份
